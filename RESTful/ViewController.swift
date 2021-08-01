c312f3f76b55c273a79cf3473153efd395e7723a
Sherry1481 <shareyscorpi1@gmail.com>
2021-08-02 02:02:41 +0500

    commit

--- RESTful/ViewController.swift
+++ RESTful/ViewController.swift
@@ -8,6 +8,19 @@
 
 import UIKit
 
+class Music {
+    static func fetch() {
+        let urlString = "https://www.google.com/"
+        
+        if let url = URL.init(string: urlString) {
+            let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+                print(String.init(data: data!, encoding: .ascii) ?? "no data")
+            }
+            task.resume()
+            }
+        }
+    }
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -15,6 +28,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        Music.fetch()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
